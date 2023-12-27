5688238b1158cc1bc5ada45d06fdee70e0d254ac
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2023-12-27 17:50:53 -0600

    Clase para generar coneccion a la bd

---
+++ SemaforizacionPorLotes/Utils/DbConnection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemaforoPorLotes.Utils
+{
+    public sealed class DbConnection
+    {
+        private SQLiteConnection connection;
+        private static DbConnection instance;
+        
+        
+        public static DbConnection Instance { 
+            get 
+            {
+                if (instance == null)
+                {
+                    instance = new DbConnection();
+                }
+                return instance;
+            }
+        }
+
+        
+        private DbConnection()
+        {
+            string connectionString = "Data Source=./semaforo.db; Version=3;";
+            connection = new SQLiteConnection(connectionString);
+            connection.Open();
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            return connection;
+        }
+
+        public void CloseConnection()
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
