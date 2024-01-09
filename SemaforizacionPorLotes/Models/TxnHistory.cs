db37388703af32865db2967abc41e3c2cd263d3c
Jonathan Hernandez Tinoco <tinocojona@outlook.com>
2024-01-09 16:40:56 -0600

    Modelo TxnHistory

---
+++ SemaforizacionPorLotes/Models/TxnHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemaforizacionPorLotes.Models
+{
+    class TxnHistory
+    {
+        public int LotNumberId { get; set; }
+        public int Quantity { get; set; }
+        public string Date { get; set; }
+
+        public TxnHistory(int lotNumberId, int quantity, string date)
+        {
+            LotNumberId = lotNumberId;
+            Quantity = quantity;
+            Date = date;
+        }
+    }
+}
